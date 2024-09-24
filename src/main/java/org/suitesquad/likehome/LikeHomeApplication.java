d4374a9261355dc8539ef4f4fb6ac6d7515b0b95
airsquared <36649395+airsquared@users.noreply.github.com>
2024-09-23 23:08:49 -0700

    Set up Spring Boot project

---
+++ src/main/java/org/suitesquad/likehome/LikeHomeApplication.java
@@ -0,0 +1,13 @@
+package org.suitesquad.likehome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikeHomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LikeHomeApplication.class, args);
+    }
+
+}
